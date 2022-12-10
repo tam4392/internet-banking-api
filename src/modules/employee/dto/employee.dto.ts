9038ad1cefa47e062fa14424a435d2b200ca41fc
liam <stdiodl@gmail.com>
2022-12-10 17:51:44 +0700

    Update employee service

--- src/modules/employee/dto/employee.dto.ts
+++ src/modules/employee/dto/employee.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsNumber, IsOptional, IsPhoneNumber, IsString } from 'class-validator';
 
 export class CreateDto {
   @IsNotEmpty()
@@ -12,4 +12,34 @@ export class CreateDto {
   @IsNotEmpty()
   @IsString()
   password: string;
+
+  @IsPhoneNumber()
+  phone: string;
+
+  @IsNumber()
+  type: number;
+}
+
+export class UpdateDto {
+  @IsNotEmpty()
+  @IsNumber()
+  id: number;
+
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  password: string;
+
+  @IsPhoneNumber()
+  phone: string;
+
+  @IsNumber()
+  type: number;
 }
